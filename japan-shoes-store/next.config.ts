49c24020f67f72403c271424049371749a5a4f94
harryrismananda <harryrismananda@gmail.com>
2025-10-07 17:53:41 +0700

    feat: update configuration for image handling and add jsonwebtoken dependencies

--- japan-shoes-store/next.config.ts
+++ japan-shoes-store/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images:{
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bo.asics.co.id',
+      }
+    ]
+  }
 };
 
 export default nextConfig;
